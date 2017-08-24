a0d80e1a9e729ef002c48f7645c37962f9acd415
atareao <lorenzo.carbonell.cerezo@gmail.com>
2017-08-24 18:52:20 +0200

    Added install_telegram_cli.sh

---
+++ install_telegram_cli.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+    echo "Run script as ROOT please. (sudo !!)"
+    exit
+fi
+
+echo "=== Updating Raspberry ==="
+apt update -y
+apt upgrade -y
+
+echo "=== Install dependencies ==="
+apt install git libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev
+apt install libevent-dev make
+
+echo "=== Cloning telegram-cli ==="
+rm -rf tg
+git clone --recursive https://github.com/vysheng/tg.git
+cd tg
+./configure
+make
+cp bin/telegram-cli /usr/bin
+cp server.pub /etc/telegram-cli/server.pub
+cd ..
+rm -rf tg
